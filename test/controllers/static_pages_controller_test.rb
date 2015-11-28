5df6afdbbae49c01fc993f6387a7d9827c8a57a1
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-11-28 20:48:40 +0500

    Fix typo in tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,18 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select 'title', 'Rails tutorial'
+    assert_select 'title', 'Rails Tutorial'
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select 'title', 'Help | Rails tutorial'
+    assert_select 'title', 'Help | Rails Tutorial'
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select 'title', 'About | Rails tutorial'
+    assert_select 'title', 'About | Rails Tutorial'
   end
 end
