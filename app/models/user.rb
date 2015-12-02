c8ab8ed07706b7f5753bee7a124404a566e5ad93
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-12-02 14:43:04 +0500

    email validations with uniqueness indexed in database

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# just the user validations
+class User < ActiveRecord::Base
+  validates :name, presence: true,
+                   length: { maximum: 100 }
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    format: { with:
+                                /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
+                    uniqueness: { case_sensitive: false }
+end
